5cfca44211ce27d905257b97a767c130404d17bd
sihai571332 <sihai571332@gmail.com>
2020-10-21 20:01:46 -0500

    Test Commit
    Signed-off-by: sihai571332 <sihai571332@gmail.com>

---
+++ lib/models/gesture.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Gesture {
+  String userId;
+  String label;
+  String xData;
+  String yData;
+  String zData;
+  int dateAdded;
+
+  Gesture(this.userId, this.label, this.xData, this.yData, this.zData,
+      this.dateAdded);
+}
